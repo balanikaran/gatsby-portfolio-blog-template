8e266af7483b167409a087945c09a36d31fb1ac0
krnblni <karanbalani007@gmail.com>
2020-05-16 17:11:16 +0530

    added basic pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import {Link} from 'gatsby'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Index Page</h1>
+            <p><Link to="/about">About</Link></p>
+            <p><Link to="/blog">Blog</Link></p>
+            <p><Link to="/contact">Contact</Link></p>
+        </div>
+    )
+}
+
+export default IndexPage
